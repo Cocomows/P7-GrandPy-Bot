62bc3e4d55ec68f3ff16129ad8abb3d557a67f60
Cocomows <corentin.caradec@gmail.com>
2018-07-11 12:18:58 +0200

    First frontend version

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello world'
+def home():
+    return render_template("pages/home.html")
+
+@app.route('/about')
+def about():
+    return render_template("pages/about.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("errors/404.html"), 404
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port=3000)
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -7,9 +7,17 @@ def client():
     app.config['TESTING'] = True
     return app.test_client()
 
-def test_page(client):
+def test_home_page(client):
 
     rsp = client.get('/')
     assert rsp.status == '200 OK'
     html = rsp.get_data(as_text=True)
-    assert 'Hello world' in html
+    assert 'GrandPy Bot' in html
+    assert '<input' in html
+
+def test_error_page(client):
+
+    rsp = client.get('/error404')
+    assert rsp.status == '404 NOT FOUND'
+    html = rsp.get_data(as_text=True)
+    assert "La page demandée n'a pas été trouvée" in html
