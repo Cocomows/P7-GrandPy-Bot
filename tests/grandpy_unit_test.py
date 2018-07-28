bddd346c3bafacc12a322bfb00e5b468e7a348b1
Cocomows <corentin.caradec@gmail.com>
2018-07-28 13:51:33 +0200

    Configuration correction

--- tests/grandpy_unit_test.py
+++ tests/grandpy_unit_test.py
@@ -11,7 +11,6 @@ class MockRequestsResponse:
         self.text = resp
 
 
-
 def test_requests_response(monkeypatch):
 
     results_wiki= '{"batchcomplete":"","continue":{"gsroffset":1,"continue":"gsroffset||"},"query":{"pageids":["681159"],"pages":{"681159":{"pageid":681159,"ns":0,"title":"Paris","index":1,"extract":"Paris (prononc\u00e9 [pa.\u0281i] ) est la capitale de la France. Elle se situe au c\u0153ur d\'un vaste bassin s\u00e9dimentaire aux sols fertiles et au climat temp\u00e9r\u00e9, le bassin parisien, sur une boucle de la Seine, entre les confluents de celle-ci avec la Marne et l\'Oise. Ses habitants s\u2019appellent les Parisiens. Paris est \u00e9galement le chef-lieu de la r\u00e9gion \u00cele-de-France et l\'unique commune fran\u00e7aise qui est en m\u00eame temps un d\u00e9partement. Commune centrale de la m\u00e9tropole du Grand Paris, cr\u00e9\u00e9e en 2016, elle est divis\u00e9e en arrondissements, comme les villes de Lyon et de Marseille, au nombre de vingt."}}}}'
